29bb66fbbcf001de37d97fae7f32dd2fbee3014b
Aaron Kessler <kessler.aaron@gmail.com>
2019-06-24 21:43:53 -0400

    rename outputs

--- 02_tidytxtattempt.R
+++ 02_tidytxtattempt.R
@@ -18,19 +18,18 @@ mueller_list <- tibble(
   select(page, line, text)
 
 
-# remove lines containing title 
-mueller_list <- mueller_list[-c(1:5),] 
-
 #save to file
-write_csv(mueller_list, "mueller_case_list.csv")
+write_csv(mueller_list, "mueller_list1.csv")
 
+# remove lines containing title 
+mueller_list2 <- mueller_list[-c(1:5),] 
 
 #remove header rows on each page
-mueller_list <- mueller_list %>% 
+mueller_list2 <- mueller_list2 %>% 
   filter(!str_detect(text, "Case 1:19-mc-00058-BAH"))
 
 #mark when a new case begins
-mueller_list2 <- mueller_list %>% 
+mueller_list2 <- mueller_list2 %>% 
   mutate(
     newcase_mark = if_else(str_detect(text, "1:17-mj"), "New Case", "")
     ) %>% 
@@ -39,7 +38,8 @@ mueller_list2 <- mueller_list %>%
     line,
     newcase_mark,
     text
-  ) 
+  ) %>% 
+  write_csv("mueller_list2.csv") 
 
 
 mueller_list3 <- mueller_list2 %>% 
